ef266af863ab71153bb1d9674159611c98679b4d
delguanzon <yodelguanzon@gmail.com>
2022-11-28 14:22:39 -0800

    add base dev

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/styles.css';
+import User from './js/user.js';
+
+// Save to Session
+// sessionStorage.setItem("key", value);
+// Get from Session
+// const varName = sessionStorage.getItem("key");
+
+function handleFormSubmission() {
+  const person = new User("name", "age", "dob");
+  //To access person, do JSON.parse(sessionStorage.getItem(person))
+  sessionStorage.setItem("person", JSON.stringify(person));
+}
 
 window.addEventListener('load', function () {
 //console.log(`${process.env.API_KEY}`);
