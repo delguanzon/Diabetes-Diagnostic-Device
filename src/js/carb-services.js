67df366cb20464f3473330cbb13e508997d16305
delguanzon <yodelguanzon@gmail.com>
2022-11-29 14:54:54 -0800

    Update carb-service.js

--- src/js/carb-services.js
+++ src/js/carb-services.js
@@ -1,11 +1,12 @@
 export default class CarbService {
   static getCarbs(food) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=68299e3f&app_key=${process.env.API_KEY}&ingr=${food}&nutrition-type=cooking`;
+      request.addEventListener("loadend", function () {
         const response = JSON.parse(this.responseText);
         if (this.status === 200) {
-          resolve ([response, food]);
+          resolve([response, food]);
         } else {
           reject([this, response, food]);
         }
@@ -15,3 +16,4 @@ export default class CarbService {
     });
   }
 }
+
