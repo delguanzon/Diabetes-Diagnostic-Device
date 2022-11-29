b2be0e39b5bc1640f88d6e29e58df2339613415c
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-29 09:30:28 -0800

    add calculateA1C functionality
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- src/js/blood-glucose.js
+++ src/js/blood-glucose.js
@@ -26,15 +26,25 @@ export function addInsulinLevel(bloodILevel, dateTime) {
   // Store number & time of day in User object session storage
 }
 
-export function calculateA1C(totalBloodGLevel) {
-  console.log(totalBloodGLevel);
-  // * A1C% = (Estimated average glucose(mg/dL) + 46.7) / 28.7 
+export function calculateA1C() {
+  // Get blood glucose level array
+  let user = JSON.parse(sessionStorage.getItem('person'));
+  let glucLvls = user.glucoseLevels;
+  // Average bgl array
+  let total = 0;
+  glucLvls.forEach((element, index) => {
+    const gLvl = parseInt(element);
+    console.log(index, element);
+    total += gLvl;
+    console.log(total);
+  });
+  let avg = total / (glucLvls.length);
+  console.log(avg);
+  // Calc A1C
+  let a1C = (avg + 46.7) / 28.7;
+  console.log(a1C);
+  // Add A1C to user obj
+  user.a1C = a1C.toFixed(2);
   // Store in user object session storage
+  sessionStorage.setItem('person', JSON.stringify(user));
 }
-
-// Blood Glucose Monitor
-  // * User set goal for glucose level
-  // * Blood Glucose - User inputted number, five times / day or avg
-    //   * store  inputted
-    //   * (morning, breakfast, lunch, dinner, bedtime)
-  // * Insulin Units/day - User inputted number based on what you eat / what your levels are
