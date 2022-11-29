9161b7c8dbebd495cc530ddc1800a29d9cfe4fbc
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-29 11:32:47 -0800

    WIP: add dataToTable function, TODO: refactor printData fx
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- src/index.js
+++ src/index.js
@@ -34,7 +34,51 @@ function handleGlucoseSubmission() {
   addInsulinLevel(insLvl, insLvlTime);
   calculateA1C();
   printGlucoseData();
+  testPrintGlucTables()
   printInsulinData();
+}
+
+function dataToTable(array1Name, array1, array2Name, array2 ) {
+  // Add data to table
+  let table = document.createElement('table');
+  table.setAttribute('class', 'table');
+  // Build table header
+  let head = document.createElement('thead');
+  let headerRow = document.createElement('tr');
+  let tableHeader1 = document.createElement('th');
+  let tableHeader2 = document.createElement('th');
+  tableHeader1.innerText = array1Name;
+  tableHeader2.innerText = array2Name;
+  headerRow.append(tableHeader1, tableHeader2);
+  head.append(headerRow);
+  table.append(head);
+  // Build table body
+  let body = document.createElement('tbody');
+  for (let i = 0; i < array1.length; i++) {
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    td1.innerText = array1[i];
+    td2.innerText = array2[i];
+    tr.append(td1, td2);
+    body.append(tr);
+  }
+  table.append(body);
+  return table;
+}
+
+function testPrintGlucTables() {
+  let user = JSON.parse(sessionStorage.getItem('person'));
+  // Make new div for data if not already made
+  if (!document.querySelector('div#glucDiv')) {
+    let glucDiv = document.createElement('div');
+    glucDiv.setAttribute('id', 'glucDiv');
+    document.querySelector('div.container').append(glucDiv);
+  } else {
+    document.querySelector('div#glucDiv').replaceChildren("");
+  }
+  const table = dataToTable('Glucose Levels', user.glucoseLevels, 'Glucose Time', user.glucoseTimes);
+  document.querySelector('div#glucDiv').append(table);
 }
 
 function printGlucoseData() {
@@ -73,7 +117,7 @@ function printGlucoseData() {
     body.append(tr);
   }
   table.append(body);
-  document.querySelector('div#glucDiv').append(table);
+  // document.querySelector('div#glucDiv').append(table);
 }
 
 function printInsulinData() {
@@ -85,6 +129,7 @@ function printInsulinData() {
   } else {
     document.querySelector('div#insDiv').replaceChildren('');
   }
+  // Table functionality
   let table = document.createElement('table');
   table.setAttribute('class', 'table');
   let head = document.createElement('thead');
