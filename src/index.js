447b70a4e292b304e12fd21216314d2ab8b257ad
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-28 16:06:36 -0800

    add handleGlucoseSubmission & hard-coded handleFormSub to window on load

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/styles.css';
 import User from './js/user.js';
+import {updateGlucoseGoal} from './js/blood-glucose.js'
 
 // Save to Session
 // sessionStorage.setItem("key", value);
@@ -14,6 +15,25 @@ function handleFormSubmission() {
   sessionStorage.setItem("person", JSON.stringify(person));
 }
 
+function handleGlucoseSubmission() {
+  // Retrieve inputs
+  const glucGoal = document.getElementById('glucose-goal').value;
+  const glucLvl = document.getElementById('glucose-level').value;
+  const glucLvlTime = document.getElementById('glucose-time').value;
+  const insLvl = document.getElementById('insulin-level').value;
+  const insLvlTime = document.getElementById('insulin-time').value;
+  console.log(glucGoal);
+  console.log(glucLvl);
+  console.log(glucLvlTime);
+  console.log(insLvl);
+  console.log(insLvlTime);
+  // Run functions to add data to user object
+  updateGlucoseGoal(glucGoal);
+
+}
+
 window.addEventListener('load', function () {
 //console.log(`${process.env.API_KEY}`);
+  handleFormSubmission();
+  document.querySelector('form#glucose-level').addEventListener('submit', handleGlucoseSubmission);
 });
