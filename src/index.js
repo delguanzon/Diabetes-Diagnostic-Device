1c3393d7a30ec837103e176310f798f5e6e3477e
delguanzon <yodelguanzon@gmail.com>
2022-11-29 08:53:35 -0800

    initial activity form

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import User from './js/user.js';
 // Get from Session
 // const varName = sessionStorage.getItem("key");
 
-function handleFormSubmission() {
+// function handleFormSubmission() {
+//   const person = new User("name", "age", "dob");
+//   //To access person, do JSON.parse(sessionStorage.getItem(person))
+//   sessionStorage.setItem("person", JSON.stringify(person));
+// }
+
+window.addEventListener('load', function () {
   const person = new User("name", "age", "dob");
   //To access person, do JSON.parse(sessionStorage.getItem(person))
   sessionStorage.setItem("person", JSON.stringify(person));
-}
-
-window.addEventListener('load', function () {
-//console.log(`${process.env.API_KEY}`);
 });
